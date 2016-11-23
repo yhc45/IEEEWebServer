528c4de75745b773e21f85ef93527121951c7b46
ojimenez1145 <o4jimene@ucsd.edu>
2016-11-23 10:26:12 -0800

    Update ReadSensorFinal.py

--- ReadSensorFinal.py
+++ ReadSensorFinal.py
@@ -2,8 +2,6 @@ import sys
 import Adafruit_DHT
 import time
 import pymongo
-from datetime import datetime
-from pytz import timezone
 
 ##should we put everything below in a function main() as called at the end??
 
@@ -21,10 +19,6 @@ def main():
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
 
    while True:
-      dto = datetime.now(timezone('UTC'))
-      dto_pacific = dto.astimezone(timezone('US/Pacific')) #.localize(dto)
-      dts = datetime.strftime(dto_pacific,"%Y-%m-%d %H:%M:%S")
-
 
       humidity, temperature = Adafruit_DHT.read_retry(11, 4)
       temperature = temperature * 9/5.0 + 32
@@ -41,7 +35,7 @@ def main():
       time.sleep(measure_interval);
 
 def connectMongo():
-  connection = pymongo.MongoClient("mongodb://<dbuser>:<dbpassword>@ds019746.mlab.com:19746/qpfallteam7")
+  connection = pymongo.MongoClient("mongodb://admin:admin@ds019746.mlab.com:19746/qpfallteam7")
   db = connection.qpfallteam7
   return db
 
